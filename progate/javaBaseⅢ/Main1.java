049b95d460db259c4af7f6ff32532ea504ef610c
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-10-21 18:34:32 +0900

    java基礎Ⅲ

---
+++ progate/javaBaseⅢ/Main1.java
@@ -0,0 +1,12 @@
+package javaBaseⅢ;
+
+class Main1 {
+  public static void main(String[] args) {
+    hello();
+  }
+
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
+  }
+}
