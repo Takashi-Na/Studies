40889a0c9a491ea095ff33f03f0bc9ec3215cafa
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-11-22 20:57:05 +0900

    javaⅣ

--- progate/javaⅣ/Main4.java
+++ progate/javaⅣ/Main4.java
@@ -1,5 +1,14 @@
 package javaⅣ;
 
 class Main4 {
-  
+  public static void main(String[] args) {
+    Person4 person1 = new Person4();
+    // person1に対して、helloメソッドを呼び出してください
+    person1.hello();
+
+    Person4 person2 = new Person4();
+    // person2に対して、helloメソッドを呼び出してください
+    person2.hello();
+
+  }
 }
--- progate/javaⅣ/Person4.java
+++ progate/javaⅣ/Person4.java
@@ -1,5 +1,7 @@
 package javaⅣ;
 
 class Person4 {
-  
+  public void hello() {
+    System.out.println("こんにちは");
+  }
 }
