e9d77d945d597de7e8623f93d30ecde4770e085f
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-11-25 11:36:40 +0900

    javaⅣ

---
+++ progate/javaⅣ/Main6.java
@@ -0,0 +1,13 @@
+package javaⅣ;
+
+class Main6 {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    person1.name = "Kate Jones";
+    person1.hello();
+
+    Person person2 = new Person();
+    person2.name = "John Christopher Smith";
+    person2.hello();
+  }
+}
---
+++ progate/javaⅣ/Perosn6.java
@@ -0,0 +1,10 @@
+package javaⅣ;
+
+class Perosn6 {
+  public String name;
+
+  public void hello() {
+    // thisを用いて、「こんにちは、私は◯◯です」で出力するように以下を書き換えてください
+    System.out.println("こんにちは、私は" + this.name + "です");
+  }
+}
