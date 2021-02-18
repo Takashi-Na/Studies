3b0aca997070ac0bf7bf62a87287f03e769b04ad
Takashi-Na <Takashi-Na@users.noreply.github.com>
2021-02-18 17:39:34 +0900

    char型

---
+++ C/苦C/char_array_disp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+  char str[] = { 'M', 'A', 'R', 'I', 'O', '\0'};
+  printf("%s\n", str);
+  return 0;
+}
---
+++ C/苦C/char_cv.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char c = '8'; //数字
+  int num_value = c - '0'; //数値に変換
+  printf("%d\n", num_value);
+  return 0;
+}
